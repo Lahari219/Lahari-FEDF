286a76678d2f87e6e0c16fea3325dedc15ccdc4c
Lahari219 <2420030206@klh.edu.in>
2025-10-08 09:00:37 +0530

    Add files via upload

---
+++ week7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "Treasure Island", author: "Robert Louis Stevenson", price: 1499, availability: "in stock" },
+  { title: "The Secret Garden", author: "Frances Hodgson Burnett", price: 1699, availability: "in stock" },
+  { title: "The Wizard of Oz", author: "L. Frank Baum", price: 1599, availability: "out of stock" },
+  { title: "Nevermoor", author: "Jessica Townsend", price: 1450, availability: "in stock" }
+];
